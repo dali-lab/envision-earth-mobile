a488990da06ca36b6a8e00d6886d0a197947e978
rishavchakra <rishavchakra@gmail.com>
2022-10-27 21:50:18 -0400

    Style guide colors

--- src/utils/styles/Colors.tsx
+++ src/utils/styles/Colors.tsx
@@ -1,24 +1,52 @@
+// const colorsOld = {
+//   primaryOld: {
+//     light: '#05C3DD',
+//     normal: '#26AADE',
+//     dark: '#0077A8',
+//   },
+//   secondaryOld: {
+//     light: '#E0F2E1',
+//     normal: '#A7DBAA',
+//     dark: '#69CD6F',
+//   },
+//   neutral: {
+//     1: '#E2E7EC',
+//     2: '#D6DDE4',
+//     3: '#CDD4DA',
+//     4: '#B1BECC',
+//     5: '#949FAF',
+//     6: '#707A89',
+//     7: '#424B59',
+//     8: '#2A2F38',
+//   },
+//   errorOld: '#FF4D3D',
+//   highlight: '#FFD700',
+// };
+
 export default {
   primary: {
-    light: '#05C3DD',
-    normal: '#26AADE',
-    dark: '#0077A8',
+    lightestGreen: '#F1F5E8',
+    lightGreen: '#E0E6CE',
+    vibrantGreen: '#82A920',
+    deepGreen: '#525548',
+    lightOrange: '#FAECD5',
+    mainOrange: '#EDAC4B',
   },
   secondary: {
-    light: '#E0F2E1',
-    normal: '#A7DBAA',
-    dark: '#69CD6F',
+    white: '#FFFFFF',
+    mediumGreen: '#AAB888',
+    brown: '#39190E',
+    teal: '#3F7788',
+    yellow: '#F2C376',
+    lightTeal: '#B0D1CD',
   },
-  neutral: {
-    1: '#E2E7EC',
-    2: '#D6DDE4',
-    3: '#CDD4DA',
-    4: '#B1BECC',
-    5: '#949FAF',
-    6: '#707A89',
-    7: '#424B59',
-    8: '#2A2F38',
+  accent: {
+    blue: '#00C2FF',
+    green: '#1B7E73',
   },
-  error: '#FF4D3D',
-  highlight: '#FFD700',
+  error: {
+    bright: '#953B24',
+    dim: '#E18D85',
+    muted: '#FBF1F0',
+  },
 };
